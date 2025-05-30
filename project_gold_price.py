b0b6b4b935e4cf31df3a1e160020185927bd2323
MrTushar4455 <162832876+MrTushar4455@users.noreply.github.com>
2025-05-30 15:36:05 +0530

    Add files via upload

--- project_gold_price.py
+++ project_gold_price.py
@@ -17,14 +17,16 @@ df = df.sort_values("date")
 
 arima_model = joblib.load("ARIMA_model.pkl")
 
+forecast_days = st.number_input("Enter number of days to forecast", min_value=1, max_value=365, value=30, step=1)
+
 # Helper: create lag features
-def create_lag_features(series, n_lags=30):
+def create_lag_features(series, n_lags=forecast_days):
     return np.array(series[-n_lags:]).reshape(1, -1)
 
 # Forecast functions
 
 
-def forecast_arima(model, df, days=30):
+def forecast_arima(model, df, days=forecast_days):
     forecast_values = model.forecast(steps=days)
     start_date = df['date'].max() + timedelta(days=1)
     forecast_dates = [start_date + timedelta(days=i) for i in range(days)]
